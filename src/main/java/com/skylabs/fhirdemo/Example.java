c660d8ab379ee267eaaddda37ac7d895dc472a2b
한봉훈 <hanbonghun@hanbonghun-ui-MacBookPro.local>
2025-03-27 17:52:06 +0900

    프로젝트 생성

---
+++ src/main/java/com/skylabs/fhirdemo/Example.java
@@ -0,0 +1,10 @@
+package com.skylabs.fhirdemo;
+
+import ca.uhn.fhir.context.FhirContext;
+
+public class Example {
+    
+    public static void main(String[] args) {
+        FhirContext fhirContext = FhirContext.forDstu2();
+    }
+}
